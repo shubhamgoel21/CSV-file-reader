4e841e647b7ff4701a32c968c788300efe545f86
shubham <shubhamgoel2100@gmail.com>
2022-12-08 19:50:40 +0530

    mvc structure has been contructed

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,4 +1,5 @@
 module.exports.home=function(req,res)
 {
-           return res.send("linked routes and controller");
+           // return res.send("linked routes and controller");
+           return res.render("home");
 }
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express=require("express");
 const port=8000;
+const path=require("path");
+var expressLayouts = require('express-ejs-layouts');
 
 const app=express();
+
+app.use(expressLayouts);
+app.set("layout extractScripts", true)
+app.set("layout extractStyles", true)
+
+app.use(express.static('assets'))
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use('/',require("./routes"));
 
